753b6aa3d32168888d251df441d387bafd36bf6a
Antoine <antoine.marie62126@gmail.com>
2023-02-15 20:23:28 +0100

    condition pour savoir si il ya une table user et des user dans la bdd

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
 
@@ -12,6 +15,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -24,35 +28,120 @@ const createWindow = () => {
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
 
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quitter l'application lorsque toutes les fenêtres sont fermées, sauf sur macOS où il est courant pour les applications et leur barre de menu de rester actives jusqu'à ce que l'utilisateur quitte explicitement en utilisant Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
 
 
+
+// base de donnée
+
+//création de la bdd
 let db = new sqlite3.Database('./myDatabase.db', (err) => {
   if (err) {
     console.error(err.message);
   }
   console.log('Connected to the database.');
-});
 
-app.on('ready', () => {
- db.run('CREATE TABLE users (id INT, name TEXT)');
+
+
+
+  const tableExistsQuery = `SELECT name FROM sqlite_master WHERE type='table' AND name='user';`;
+  const getUserQuery = `SELECT * FROM user LIMIT 1;`;
+
+  db.get(tableExistsQuery, (err, row) => {
+    if (err) {
+      console.error(err.message);
+      // En cas d'erreur, afficher un message d'erreur
+      console.log('Erreur lors de la vérification de la table user dans la base de données.');
+    } else {
+      if (row) {
+        console.log('La table user existe dans la base de données.');
+        // Vérifier si la table contient déjà un utilisateur
+        db.get(getUserQuery, (err, row) => {
+          if (err) {
+            console.error(err.message);
+            // En cas d'erreur, afficher un message d'erreur
+            console.log("Erreur lors de la récupération d'un utilisateur depuis la table user.");
+          } else {
+            if (row) {
+              console.log('Un utilisateur existe déjà dans la table user.');
+              // Si un utilisateur existe, rediriger vers index.html
+              const {
+                app,
+                BrowserWindow
+              } = require('electron');
+              let mainWindow = new BrowserWindow({
+                width: 800,
+                height: 600
+              });
+              mainWindow.loadFile('index.html');
+            } else {
+              console.log("Aucun utilisateur n'existe dans la table user.");
+              // Si la table ne contient pas d'utilisateur, rediriger vers register.html
+              const {
+                app,
+                BrowserWindow
+              } = require('electron');
+              let mainWindow = new BrowserWindow({
+                width: 800,
+                height: 600
+              });
+              mainWindow.loadFile('pages/register.html');
+            }
+          }
+        });
+      } else {
+        console.log("La table user n'existe pas dans la base de données.");
+        // Si la table n'existe pas, créer la table user et rediriger vers register.html
+        const createTableQuery = `CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, password TEXT NOT NULL);`;
+        db.run(createTableQuery, (err) => {
+          if (err) {
+            console.error(err.message);
+            // En cas d'erreur, afficher un message d'erreur
+            console.log("Erreur lors de la création de la table user dans la base de données.");
+          } else {
+            console.log('La table user a été créée dans la base de données.');
+            // Rediriger vers register.html
+            const {
+              app,
+              BrowserWindow
+            } = require('electron');
+            let mainWindow = new BrowserWindow({
+              width: 800,
+              height: 600
+            });
+            mainWindow.loadFile('pages/register.html');
+          }
+        });
+      }
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  app.on('ready', () => {
+
+
+
+  })
 });
